63384626ca6aaaea0d93f11b4a6497fa2052b944
Claire Shen <yutongs2@andrew.cmu.edu>
2026-03-08 06:38:02 -0400

    Initial Commit

---
+++ onbroading_remain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  onbroading_remain
+//
+//  Created by Yutong on 3/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
