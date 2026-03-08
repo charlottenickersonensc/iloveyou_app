ae04f5d6605fbb523aa2f011d6c93b7a2499a19a
Claire Shen <yutongs2@andrew.cmu.edu>
2026-03-07 20:55:22 -0500

    Initial Commit

---
+++ welcome+intro_page/welcome_intro_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  welcome_intro_pageApp.swift
+//  welcome+intro_page
+//
+//  Created by Yutong on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct welcome_intro_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
